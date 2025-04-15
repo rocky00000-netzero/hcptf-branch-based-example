0e5b8e91bc8b2660e5af6eced4da72a903a95a99
rocky00000-netzero <luv.ps.msc.glb.com@gmail.com>
2025-04-15 11:40:26 +0900

    test

---
+++ main.tf
@@ -0,0 +1,50 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.7.1"
+    }
+  }
+}
+
+resource "random_pet" "example_one" {
+  length    = var.pet_length
+  prefix    = var.pet_prefix
+  separator = var.pet_separator
+}
+
+resource "random_pet" "example_two" {
+  length    = var.pet_length
+  prefix    = var.pet_prefix
+  separator = var.pet_separator
+}
+
+resource "random_pet" "example_three" {
+  length    = var.pet_length
+  prefix    = var.pet_prefix
+  separator = var.pet_separator
+}
+
+resource "random_pet" "example_four" {
+  length    = var.pet_length
+  prefix    = var.pet_prefix
+  separator = var.pet_separator
+}
+
+resource "random_pet" "example_five" {
+  length    = var.pet_length
+  prefix    = var.pet_prefix
+  separator = var.pet_separator
+}
+
+resource "random_pet" "example_six" {
+  length    = var.pet_length
+  prefix    = var.pet_prefix
+  separator = var.pet_separator
+}
+
+resource "random_pet" "example_seven" {
+  length    = var.pet_length
+  prefix    = var.pet_prefix
+  separator = var.pet_separator
+}
---
+++ output.tf
@@ -0,0 +1,27 @@
+output "pet_example_one_id" {
+  value = random_pet.example_one.id
+}
+
+output "pet_example_two_id" {
+  value = random_pet.example_two.id
+}
+
+output "pet_example_three_id" {
+  value = random_pet.example_three.id
+}
+
+output "pet_example_four_id" {
+  value = random_pet.example_four.id
+}
+
+output "pet_example_five_id" {
+  value = random_pet.example_five.id
+}
+
+output "pet_example_six_id" {
+  value = random_pet.example_six.id
+}
+
+output "pet_example_seven_id" {
+  value = random_pet.example_six.id
+}
